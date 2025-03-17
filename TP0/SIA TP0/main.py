254f90b3bed047ba0230f5cb2ad64cce2e3ac0ab
jfelipeper <juanfelipeperdomorojas@gmail.com>
2025-03-16 23:29:38 -0300

    Solve Point 2a

--- TP0/SIA TP0/main.py
+++ TP0/SIA TP0/main.py
@@ -6,6 +6,7 @@ from src.pokemon import PokemonFactory, StatusEffect
 N = 100
 balls = ["pokeball", "ultraball", "fastball", "heavyball"]
 pokemon_configs = ["TP0\SIA TP0\configs\caterpie.json", "TP0\SIA TP0\configs\snorlax.json"]
+status_effects = [StatusEffect.NONE, StatusEffect.BURN, StatusEffect.FREEZE, StatusEffect.PARALYSIS, StatusEffect.POISON, StatusEffect.SLEEP]
 
 if __name__ == "__main__":
     factory = PokemonFactory("TP0\SIA TP0\pokemon.json")
@@ -13,24 +14,25 @@ if __name__ == "__main__":
     for config_file in pokemon_configs:
         with open(config_file, "r") as f:
             config = json.load(f)
-            pokemon = factory.create(config["pokemon"], 100, StatusEffect.NONE, 1)
-            
-            results = {ball: 0 for ball in balls}
-            for ball in balls:
-                success_count = 0
-                for i in range(N):
-                    caught, _ = attempt_catch(pokemon, ball)
-                    if caught:
-                        success_count += 1
-                results[ball] = success_count / N
+            for status in status_effects:
+                pokemon = factory.create(config["pokemon"], 100, status, 1)
+                
+                results = {ball: 0 for ball in balls}
+                for ball in balls:
+                    success_count = 0
+                    for i in range(N):
+                        caught, _ = attempt_catch(pokemon, ball)
+                        if caught:
+                            success_count += 1
+                    results[ball] = success_count / N
 
-        print(f"Results for {config['pokemon']}:")
-        for ball, probability in results.items():
-            print(f"Probability of catching with {ball}: {probability * 100:.2f}%")
+                print(f"Results for {config['pokemon']} with status {status.name}:")
+                for ball, probability in results.items():
+                    print(f"Probability of catching with {ball}: {probability * 100:.2f}%")
 
-        # Crear gráfico de barras
-        plt.bar(results.keys(), [prob * 100 for prob in results.values()])
-        plt.xlabel('Pokebola')
-        plt.ylabel('Probabilidad de captura (%)')
-        plt.title(f'Probabilidad de captura promedio para cada pokebola ({config["pokemon"]})')
-        plt.show()
+                # Crear gráfico de barras
+                plt.bar(results.keys(), [prob * 100 for prob in results.values()])
+                plt.xlabel('Pokebola')
+                plt.ylabel('Probabilidad de captura (%)')
+                plt.title(f'Probabilidad de captura promedio para cada pokebola ({config["pokemon"]} - {status.name})')
+                plt.show()
